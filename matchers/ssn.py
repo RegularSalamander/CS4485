dfb99de83b78ee6504be06ea6d2d271fc2a25155
Cameron Kelly <cameron.kelly@utdallas.edu>
2025-03-28 13:15:56 -0500

    Updated ssn matching to allow for asterisks

--- matchers/ssn.py
+++ matchers/ssn.py
@@ -2,4 +2,5 @@ import re
 
 
 def strip_ssn(text: str, two_way=False, sub="*ssn*"):
-    return re.sub(r"\d{3}[-]\d{2}[-]\d{4}", sub, text)
+    # matches the form 111-11-1111 and allows for digits replaced by asterisks
+    return re.sub(r"[\d*]{3}[-]\d{2}[-]\d{4}", sub, text)
